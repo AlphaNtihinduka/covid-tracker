42658eee79cae3c928899e846c48cca1c2f9c6ab
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-28 14:21:14 +0300

    Add home page setup

---
+++ src/components/Continent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../css/continent.css';
+
+const Continent = () => (
+  <div className="continent-wrapper">
+    <div className="search">
+      <input type="search" placeholder="Search Country ..." className="search-input" />
+    </div>
+    <div className="continents-container">
+      <div className="continent-container">
+        <p className="continent-title">Africa</p>
+        <p className="continent-cases">Cases: 10,203,809</p>
+        <p className="continent-deaths">Deaths: 10,203,809</p>
+        <div>
+          <button type="button" className="button">See More</button>
+        </div>
+      </div>
+      <div className="continent-container">
+        <p className="continent-title">Africa</p>
+        <p className="continent-cases">Cases: 10,203,809</p>
+        <p className="continent-deaths">Deaths: 10,203,809</p>
+        <div>
+          <button type="button" className="button">See More</button>
+        </div>
+      </div>
+      <div className="continent-container">
+        <p className="continent-title">Africa</p>
+        <p className="continent-cases">Cases: 10,203,809</p>
+        <p className="continent-deaths">Deaths: 10,203,809</p>
+        <div>
+          <button type="button" className="button">See More</button>
+        </div>
+      </div>
+      <div className="continent-container">
+        <p className="continent-title">Africa</p>
+        <p className="continent-cases">Cases: 10,203,809</p>
+        <p className="continent-deaths">Deaths: 10,203,809</p>
+        <div>
+          <button type="button" className="button">See More</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Continent;
---
+++ src/components/Continents.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Continent from './Continent';
+
+const Continents = () => (
+  <div>
+    <Continent />
+  </div>
+);
+
+export default Continents;
